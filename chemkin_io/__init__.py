6a090aa956bcbb40ff94c8acb267bbc63e01b5f3
Kevin Moore <kmoore@blueslogin1.lcrc.anl.gov>
2019-09-06 16:12:29 -0500

    update

--- chemkin_io/__init__.py
+++ chemkin_io/__init__.py
@@ -8,7 +8,7 @@ from chemkin_io import reaction
 from chemkin_io import thermo
 from chemkin_io import util
 from chemkin_io import pfit
-
+from chemkin_io import transport
 
 __all__ = [
     'species_block',
@@ -18,5 +18,6 @@ __all__ = [
     'reaction',
     'thermo',
     'util',
-    'pfit'
+    'pfit',
+    'lj'
 ]
