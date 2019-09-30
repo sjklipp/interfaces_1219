0fd8de09224c3799bec21b84c4428a38f26947ea
Stephen Klippenstein <sjk@anl.gov>
2019-09-30 18:23:46 -0500

    modified mechanism parser

--- chemkin_io/mechparser/mechanism.py
+++ chemkin_io/mechparser/mechanism.py
@@ -5,6 +5,7 @@ from io import StringIO
 import pandas
 import autoparse.pattern as app
 from automol.smiles import inchi
+from automol.inchi import smiles
 from chemkin_io.mechparser import util
 
 
@@ -58,16 +59,79 @@ def species_name_inchi_dct(csv_str):
     """ build a dictionary of name idx and inchi entry
     """
     csv_file = StringIO(csv_str)
-    data = pandas.read_csv(csv_file)
+    data = pandas.read_csv(csv_file, comment='!')
 
     spc_dct = {}
-    if hasattr(data, 'inchi'):
-        spc_dct = dict(zip(data.name, data.inchis))
-    elif hasattr(data, 'smiles'):
-        ichs = [inchi(smiles) for smiles in data.smiles]
+    if hasattr(data, 'InChi'):
+        spc_dct = dict(zip(data.name, data.InChi))
+    elif hasattr(data, 'SMILES'):
+        smiles = [inchi(smiles) for smiles in data.SMILES]
+        spc_dct = dict(zip(data.name, smiles))
+    else:
+        raise ValueError
+
+    return spc_dct
+
+
+def species_name_smiles_dct(csv_str):
+    """ build a dictionary of name idx and inchi entry
+    """
+    csv_file = StringIO(csv_str)
+    data = pandas.read_csv(csv_file, comment='!')
+
+    spc_dct = {}
+    if hasattr(data, 'SMILES'):
+        spc_dct = dict(zip(data.name, data.SMILES))
+    elif hasattr(data, 'InChi'):
+        ichs = [smiles(inchi) for ich in data.InChi]
         spc_dct = dict(zip(data.name, ichs))
     else:
         raise ValueError
+
+    return spc_dct
+
+
+def species_name_mult_dct(csv_str):
+    """ build a dictionary of name idx and inchi entry
+    """
+    csv_file = StringIO(csv_str)
+    data = pandas.read_csv(csv_file, comment='!')
+
+    spc_dct = {}
+    if hasattr(data, 'mult'):
+        spc_dct = dict(zip(data.name, data.mult))
+    else:
+        raise ValueError
+
+    return spc_dct
+
+
+def species_name_charge_dct(csv_str):
+    """ build a dictionary of name idx and inchi entry
+    """
+    csv_file = StringIO(csv_str)
+    data = pandas.read_csv(csv_file, comment='!')
+
+    spc_dct = {}
+    if hasattr(data, 'charge'):
+        spc_dct = dict(zip(data.name, data.charge))
+    else:
+        spc_dct = dict(zip(data.name, [0 for name in data.name]))
+
+    return spc_dct
+
+
+def species_name_sens_dct(csv_str):
+    """ build a dictionary of name idx and inchi entry
+    """
+    csv_file = StringIO(csv_str)
+    data = pandas.read_csv(csv_file, comment='!')
+
+    spc_dct = {}
+    if hasattr(data, 'sens'):
+        spc_dct = dict(zip(data.name, data.sens))
+    else:
+        spc_dct = dict(zip(data.name, [0. for name in data.name]))
 
     return spc_dct
 
@@ -76,7 +140,7 @@ def species_inchi_name_dct(csv_str):
     """ build a dictionary of inchi idx and name entry
     """
     csv_file = StringIO(csv_str)
-    data = pandas.read_csv(csv_file)
+    data = pandas.read_csv(csv_file, comment='!')
 
     spc_dct = {}
     if hasattr(data, 'inchi'):
