7cbd8b5e418eb39a440494c921414692c673877f
Kevin Moore <kmoore@blueslogin3.lcrc.anl.gov>
2019-10-11 00:31:28 -0500

    started pdep tolerance check for rate constants

--- ratefit/err.py
+++ ratefit/err.py
@@ -21,11 +21,39 @@ def calc_sse_and_mae(calc_ks, fit_ks):
             sse += (np.log(calc_k) - np.log(fit_k))**2.0
             abs_err.append(np.abs((calc_k - fit_k) / calc_k))
         abs_err = np.array(abs_err, dtype=np.float64)
-        mean_avg_err = np.mean(abs_err)*100.0
-        max_avg_err = np.max(abs_err)*100.0
+        mean_abs_err = np.mean(abs_err) * 100.0
+        max_abs_err = np.max(abs_err) * 100.0
     else:
         sse = None
-        mean_avg_err = None
-        max_avg_err = None
+        mean_abs_err = None
+        max_abs_err = None
 
-    return sse, mean_avg_err, max_avg_err
+    return sse, mean_abs_err, max_abs_err
+
+
+def pdep_dependence(pdep_dct, plow=None, tol1=20, tol2=20):
+    """ Assess how much the rate constants change from
+        a low-pressure regime
+    """
+    # Get a list of the sorted pressures
+    pressures = [pressure for pressure in pdep_dct
+                 if pressure != 'high']
+    pressures.sort()
+
+    # Set the lowest pressure if not specified by user
+    if plow is None:
+        plow = min(pressures)
+
+    # See changes
+    within_tolerance = True
+    for i, pressure in enumerate(pressures):
+        if i != len(pressures) + 1:
+            ktplow = pdep_dct[pressures[i]]
+            ktphigh = pdep_dct[pressures[i+1]]
+            abs_err = np.abs(ktplow - ktphigh) / ktplow
+            mean_abs_err = np.mean(abs_err) * 100.0
+            max_abs_err = np.max(abs_err) * 100.0
+            if mean_abs_err > tol1 and max_abs_err > tol2:
+                within_tolerance = False
+
+    return within_tolerance
