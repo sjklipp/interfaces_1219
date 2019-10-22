318b7ce6610d6468efc7e7ee240eea9fa64fe36a
Kevin Moore <kmoore@blueslogin4.lcrc.anl.gov>
2019-10-22 13:52:55 -0500

    update ratefit and chemkin eriter

--- varecof_io/struct/prop.py
+++ varecof_io/struct/prop.py
@@ -9,12 +9,16 @@ print(automol.geom.string(GEO))
 
 GRAPH = automol.geom.graph(GEO)
 SITES = automol.graph.resonance_dominant_radical_atom_keys(GRAPH)
-print(GRAPH)
+#print(GRAPH)
 print(SITES)
 
 GRAPH2 = ({0: ('C', 1, None), 1: ('C', 2, None), 2: ('C', 0, None)}, {frozenset({0, 2}): (1, None), frozenset({1, 2}): (1, None)})
 SITES2 = automol.graph.resonance_dominant_radical_atom_keys(GRAPH2)
-print(GRAPH2)
+#print(GRAPH2)
 print(SITES2)
 
+GEO = automol.inchi.geometry(automol.smiles.inchi('[CH3]'))
+GRAPH = automol.geom.graph(GEO)
+SITES = automol.graph.resonance_dominant_radical_atom_keys(GRAPH)
+print(SITES)
 
