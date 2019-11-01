293abebf4a569e9ed9709a85027dcff590fddfdc
Kevin Moore <kmoore@blueslogin2.lcrc.anl.gov>
2019-10-31 23:48:01 -0500

    update chemking plog writer

--- chemkin_io/writer/reaction.py
+++ chemkin_io/writer/reaction.py
@@ -12,20 +12,20 @@ def plog(reaction, rate_params_dct, temp_dct=None, err_dct=None):
     nparams = len(next(iter(rate_params_dct.values())))
     assert nparams in (3, 6)
     assert all(len(params) == nparams for params in rate_params_dct.values())
-  
+
     # Obtain a list of the pressures and sort from low to high pressure
     pressures = [pressure for pressure in rate_params_dct.keys()
                  if pressure != 'high']
     pressures.sort()
 
-    # Add fake high pressure values if they are not in the dictionary
+    # Add fake high pressure parameters if they are not in the dictionary
     if 'high' not in rate_params_dct:
         if nparams == 3:
             rate_params_dct['high'] = [1.00, 0.00, 0.00]
         elif nparams == 6:
             rate_params_dct['high'] = [1.00, 0.00, 0.00, 1.00, 0.00, 0.00]
 
-    # Next is the reaction string and high-pressure params
+    # Build the reaction string with high-pressure params and any plog params
     # Loop will build second ('DUPLICATE') section if double fit performed
     p_str = ''
     for i in range(nparams // 3):
@@ -37,10 +37,10 @@ def plog(reaction, rate_params_dct, temp_dct=None, err_dct=None):
         p_str += '{0:<32s}{1:>10.3E}{2:>9.3f}{3:9.0f} /\n'.format(
             reaction, high_a, high_n, 1000*high_ea)
 
-        # Build the PLOG string for each pressure
+        # Build the PLOG string for each pressure, other than the HighP Limit
         for pressure in pressures:
             pdep_a, pdep_n, pdep_ea = rate_params_dct[pressure][3*i:3*i+3]
-            p_str += '{0:>18s} /{1:>10.1f}  '.format(
+            p_str += '{0:>18s} /{1:>10.3f}  '.format(
                 'PLOG', float(pressure))
             p_str += '{0:>10.3E}{1:>9.3f}{2:9.0f} /\n'.format(
                 pdep_a, pdep_n, 1000*pdep_ea)
@@ -57,42 +57,39 @@ def _fit_info_str(pressures, temp_dct, err_dct):
         with the rate constant fits at each pressure
     """
 
-    # Check the temp and err dcts have same presures as rate_dcts
+    # Make temp, err dcts empty if fxn receives None; add 'high' to pressures
     temp_dct = temp_dct if temp_dct else {}
+    err_dct = err_dct if err_dct else {}
+    if 'high' in temp_dct or 'high' in err_dct:
+        pressures = ['high'] + pressures
+
+    # Check the temp and err dcts have same presures as rate_dcts
     if temp_dct:
         assert set(pressures) == set(temp_dct.keys())
     err_dct = err_dct if err_dct else {}
     if err_dct:
         assert set(pressures) == set(err_dct.keys())
-   
+
     # Write string showing the temp fit range and fit errors
-    info_str = ''
+    inf_str = '! Info Regarding Rate Constant Fits\n'
     for pressure in pressures:
         if err_dct:
             [mean_err, max_err] = err_dct[pressure]
-            err_str = '{0:15s} {1:>6.1f}%, {2:11s} {3:>6.1f}%'.format(
-                'FitMeanAbsErr =', mean_err, 'FitMaxErr =', max_err)
-            info_str += '! {0:<6s}: {1}\n'.format(str(pressure), err_str)
+            err_str = '{0:12s} {1:>5.1f}%,  {2:8s} {3:>5.1f}%'.format(
+                'MeanAbsErr =', mean_err, 'MaxErr =', max_err)
+        else:
+            err_str = ''
         if temp_dct:
             [min_temp, max_temp] = temp_dct[pressure]
-            temp_range_str = '{0:12s} {1:>.0f}-{2:<.0f} K'.format(
-                'FitTempRange =', min_temp, max_temp)
-            info_str += '! {0:<6s}: {1}\n'.format(str(pressure), temp_range_str)
+            temp_range_str = '{0:11s} {1:>.0f}-{2:<.0f} K, '.format(
+                'TempRange =', min_temp, max_temp)
+        else:
+            temp_range_str = ''
+        # Put together the who info string
+        if pressure != 'high':
+            pstr = '{0:<10.3f}'.format(pressure)
+        else:
+            pstr = '{0:<10s}'.format('High')
+        inf_str += '! {0}: {1} {2}\n'.format(pstr, temp_range_str, err_str)
 
-    return info_str
-
-
-if __name__ == '__main__':
-    reaction = 'CH3+H=CH4'
-    dct = {1.0: [1.0,2.0, 3.0, 2.0,3.0,4.0] }
-    dct2 = {1.0: [1.0,2000.0] }
-    tdct = {1.0: [100.0, 300] }
-    print(plog(reaction, dct, temp_dct=tdct, err_dct=dct2))
-    print('\n')
-    print(plog(reaction, dct, temp_dct=tdct))
-    print('\n')
-    print(plog(reaction, dct, err_dct=dct2))
-    print('\n')
-    print(plog(reaction, dct))
-    print('\n')
-
+    return inf_str
